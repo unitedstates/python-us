8a975d5662255ca48ffd7d40e2d6ff76d6068803
chebee7i <chebee7i@gmail.com>
2014-09-17 18:18:47 -0500

    Add contiguous and continental info for states.

--- us/__init__.py
+++ us/__init__.py
@@ -1,5 +1,8 @@
 from . import states
-from .states import STATES, TERRITORIES, STATES_AND_TERRITORIES, OBSOLETE
+from .states import (
+	STATES, STATES_CONTIGUOUS, STATES_CONTINENTAL,
+	TERRITORIES, STATES_AND_TERRITORIES, OBSOLETE
+)
 from .unitedstatesofamerica import *
 
 __appname__ = 'us'
