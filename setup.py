cd48bcab4673e8c89d5ab8da7049743fe9b5f153
Jeremy Carbaugh <jeremy@jcarbaugh.com>
2019-11-27 02:34:30 -0500

    drop Python 3.5 support

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     platforms=['any'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
