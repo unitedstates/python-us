fd9d577ee657e03445f237b1290707d2f5fbd52b
Jeremy Carbaugh <jeremy@jcarbaugh.com>
2019-11-27 02:28:09 -0500

    Python 3.4 is no longer supported

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     platforms=['any'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
