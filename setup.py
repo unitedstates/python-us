18d464c44d4e1f432188e1215ce25706bff608cb
Jeremy Carbaugh <jeremy@jcarbaugh.com>
2020-04-19 00:09:29 -0400

    update author_email

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 from setuptools import setup, find_packages
 
-long_description = open('README.rst').read()
+long_description = open("README.rst").read()
 
 setup(
-    name='us',
-    version='2.0.0',
+    name="us",
+    version="2.0.0",
     author="Jeremy Carbaugh",
-    author_email="jeremy@isl.co",
-    url='https://github.com/unitedstates/python-us',
-    description='US state meta information and other fun stuff',
+    author_email="jeremy@jcarbaugh.com",
+    url="https://github.com/unitedstates/python-us",
+    description="US state meta information and other fun stuff",
     long_description=long_description,
-    license='BSD',
+    license="BSD",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-         'jellyfish==0.7.2'
-    ],
-    entry_points={
-        'console_scripts': ['states = us.cli.states:main']},
-    platforms=['any'],
+    install_requires=["jellyfish==0.7.2"],
+    entry_points={"console_scripts": ["states = us.cli.states:main"]},
+    platforms=["any"],
     classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
